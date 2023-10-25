b258947d0c5644af879d67667b9a93fbc0551805
Sasha J. Kramer <34816171+sashajane19@users.noreply.github.com>
2023-10-25 10:17:44 -0700

    Update gsm_invert.m
    Update aph parameterization.

--- gsm_invert.m
+++ gsm_invert.m
@@ -1,5 +1,6 @@
-function IOPs = gsm_invert(rrs,aw,bbw,bbpstar,aphstar,admstar,pnb);
-% See: Maritorena et al. 2002 (GSM) for more details
+function IOPs = gsm_invert(rrs,aw,bbw,bbpstar,A,B,admstar,pnb);
+% See: Kramer et al. (2022) for more details
+% Note that aph parameterization is different from Maritorena et al. (2002)
 
 options = optimset('fminsearch');
 options = optimset(options,'TolX',1e-9,'TolFun',1e-9, 'MaxFunEvals', 2000, 'MaxIter', 2000);
@@ -11,7 +12,7 @@ IOPSinit=[.02,.01,.0029];
 for i = 1:size(rrs,1)
    rrs_obs = rrs(i,:);
    [iops,cost(i),exitFlag] = fminsearch('gsm_cost',IOPSinit, ...
-					options,rrs_obs,aw,bbw,bbpstar,aphstar,admstar); 
+					options,rrs_obs,aw,bbw,bbpstar,A,B,admstar); 
 
    if (exitFlag==1) % if converged then use value as IOP and inital guess
      IOPs(i,:)=iops; 
