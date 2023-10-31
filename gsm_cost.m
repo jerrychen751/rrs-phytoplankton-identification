fb292c34d2104aae2e72507e4de728fc14d1d8b3
Sasha J. Kramer <34816171+sashajane19@users.noreply.github.com>
2023-10-31 10:49:05 -0700

    Update gsm_cost.m

--- gsm_cost.m
+++ gsm_cost.m
@@ -2,7 +2,7 @@ function cost=gsm_cost(IOPs,rrs,aw,bbw,bbpstar,A,B,admstar)
 
 g = [0.0949 0.0794];	%orig., constants in eq. 2 Gordon et al., 1988
 
-a = aw + (IOPs(1).*A.^B) + IOPs(2) * admstar; %original
+a = aw + (A.*IOPs(1).^B) + IOPs(2) * admstar; %original
 bb = bbw + IOPs(3)*bbpstar;
 x = bb./(a + bb);
 rrspred = (g(1) + g(2)*x).*x;
