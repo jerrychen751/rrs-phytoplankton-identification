9f8f488fc579736714c0848c3bcbbbbcdd30d3f8
Sasha J. Kramer <34816171+sashajane19@users.noreply.github.com>
2024-10-17 16:58:03 -0700

    Update gsm_cost.m
    Separating aph and a parameterizations.

--- gsm_cost.m
+++ gsm_cost.m
@@ -2,7 +2,8 @@ function cost=gsm_cost(IOPs,rrs,aw,bbw,bbpstar,A,B,admstar)
 
 g = [0.0949 0.0794];	%orig., constants in eq. 2 Gordon et al., 1988
 
-a = aw + A.*IOPs(1).^B + IOPs(2) * admstar; %original was IOPs(1)*aphstar - check version
+aph = A.*IOPs(1).^B;
+a = aw + aph + (IOPs(2)*admstar);
 bb = bbw + IOPs(3)*bbpstar;
 x = bb./(a + bb);
 rrspred = (g(1) + g(2)*x).*x;
